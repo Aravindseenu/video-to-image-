10dceea2a055ee705f0385ff5104ca884cebbf30
AK <32041297+Aravindseenu@users.noreply.github.com>
2021-02-03 16:21:58 +0100

    Update videotoimage.py

--- videotoimage.py
+++ videotoimage.py
@@ -1,5 +1,5 @@
 import cv2
-vidcap = cv2.VideoCapture('/home/nvidia/lanenet-lane-detection/data/tusimple_test_image/test2.mp4')
+vidcap = cv2.VideoCapture('/home/nvidia/lanenet-lane-detection/data/tusimple_test_image/test2.mp4') #// enter the path to video file or give wewbcam as input
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
@@ -7,7 +7,7 @@ def getFrame(sec):
         cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
     return hasFrames
 sec = 0
-frameRate = 1 #//it will capture image in each 0.5 second
+frameRate = 1 #//change this to required number of frames the video needs to be processed 
 count=1
 success = getFrame(sec)
 while success:
